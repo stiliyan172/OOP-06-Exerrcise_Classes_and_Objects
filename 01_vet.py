bd2fcc5ccf689a94bc9c23ad426eb8d7abfcfec6
Stiliyan St <65528270+stiliyan172@users.noreply.github.com>
2022-06-02 13:03:06 +0300

    Add files via upload

---
+++ 01_vet.py
@@ -0,0 +1,40 @@
+class Vet:
+    space_left_in_clinic = 5
+    total_animals = []
+
+    def __init__(self, name):
+        self.name = name
+        self.animals = []
+
+    def register_animal(self, animal_name):
+        if self.space_left_in_clinic > 0:
+            self.animals.append(animal_name)
+            self.total_animals.append(animal_name)
+            self.space_left_in_clinic -= 1
+            return f"{animal_name} registered in the clinic"
+        return "Not enough space"
+
+    def unregister_animal(self, animal_name):
+        if animal_name in self.animals:
+            self.animals.remove(animal_name)
+            self.total_animals.remove(animal_name)
+            return f"{animal_name} unregistered successfully"
+        return f"{animal_name} not in the clinic"
+
+    def info(self):
+        return f"{self.name} has {len(self.animals)} animals. {5 - len(self.total_animals)} space left in clinic"
+
+
+peter = Vet("Peter")
+george = Vet("George")
+print(peter.register_animal("Tom"))
+print(george.register_animal("Cory"))
+print(peter.register_animal("Fishy"))
+print(peter.register_animal("Bobby"))
+print(george.register_animal("Kay"))
+print(george.unregister_animal("Cory"))
+print(peter.register_animal("Silky"))
+print(peter.unregister_animal("Molly"))
+print(peter.unregister_animal("Tom"))
+print(peter.info())
+print(george.info())
